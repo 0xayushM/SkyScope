2395e107f0fd4877ea4f08a6aeff9e378e6a96b7
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-10-06 15:03:05 +0530

    'feat:added switch between celsius and fahrenheit'

--- src/components/weatherApp.jsx
+++ src/components/weatherApp.jsx
@@ -12,26 +12,33 @@ const WeatherApp = () => {
     const [weatherData, setWeatherData] = useState({
         humidity: '',
         windSpeed: '',
-        temperature: '',
+        temperatureCelsius: '',
+        temperatureFahrenheit: '',
         location: '',
         description: '',
     });
 
     const [weatherIcon, setWeatherIcon] = useState(cloudIcon);
 
+    const [isCelsius, setIsCelsius] = useState(true); // State to track temperature unit
+
     const apiKey = 'aba312ea73142cab0213a2fb67c8f94a';
 
     const fetchData = async (city) => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${apiKey}`;
+        const unit = isCelsius ? 'metric' : 'imperial'; // Use 'imperial' for Fahrenheit
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
 
         if (response.ok) {
+            const temperatureCelsius = `${data.main.temp}°C`;
+            const temperatureFahrenheit = `${(data.main.temp * 9/5 + 32).toFixed(2)}°F`; // Convert to Fahrenheit
             setWeatherData({
                 humidity: `${data.main.humidity}%`,
                 windSpeed: `${data.wind.speed} km/h`,
-                temperature: `${data.main.temp}°C`,
+                temperatureCelsius: temperatureCelsius,
+                temperatureFahrenheit: temperatureFahrenheit,
                 location: data.name,
                 description: data.weather[0].description,
             });
@@ -73,6 +80,11 @@ const WeatherApp = () => {
         fetchData(element.value);
     };
 
+    // Function to toggle between Celsius and Fahrenheit
+    const toggleTemperatureUnit = () => {
+        setIsCelsius(!isCelsius);
+    };
+
     return (
         <div className='h-full min-h-screen p-5 flex flex-col sm:flex-row items-center justify-center gap-5 '>
             <div className='sm:w-2/3 gap-5 flex flex-col w-full max-w-[400px] tracking-wide'>
@@ -92,7 +104,12 @@ const WeatherApp = () => {
             <div className='sm:w-2/3 flex flex-col gap-5 h-full w-full max-w-[400px] tracking-wide'>
                 <div className='flex gap-5 sm:h-[150px] flex-col sm:flex-row w-full'>
                     <div className='backgroundBox flex items-center justify-center rounded-xl w-full p-5 shadow-xl text-white font-extrabold text-3xl text-center sm:w-2/3'>{weatherData.location}</div>
-                    <div className='backgroundBox rounded-xl p-5 shadow-xl text-white font-extrabold text-4xl flex justify-center items-center text-center sm:w-2/3'>{weatherData.temperature}</div>
+                    <div className='backgroundBox flex-col rounded-xl p-5 shadow-xl text-white font-extrabold text-4xl flex justify-center items-center text-center sm:w-2/3'>
+                        <div>{isCelsius ? weatherData.temperatureCelsius : weatherData.temperatureFahrenheit}</div>
+                    <button onClick={toggleTemperatureUnit} className='toggle-button text-sm'>
+                    {isCelsius ? 'Switch to Fahrenheit' : 'Switch to Celsius'}
+                </button>
+                    </div>
                 </div>
                 <div className='backgroundBox rounded-xl shadow-xl flex justify-between text-white font-bold tracking-wide p-5'>
                     <img src={humidityIcon} alt='' className='icon' />
